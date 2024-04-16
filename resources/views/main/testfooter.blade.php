13204afac7be85385f714c340d0b67f97c3d7ce7
Ilya <ilia.kozlov.y@mail.ru>
2024-04-16 23:51:41 +0700

    FEATURE: Header and Footer

--- resources/views/main/testfooter.blade.php
+++ resources/views/main/testfooter.blade.php
@@ -9,40 +9,40 @@
 				</a>
 			</div>
 			<div class="row mb-3">
-				<div class="p-2 col-md-3 col-6 d-flex flex-column">
+				<div class="p-2 col-md-3 col-6 d-flex flex-column text-center">
 					<h5>Компания</h5>
 					<a href="#">О нас</a>
 					<a href="#">Мы на карте</a>
 					<a href="#">Работа у нас</a>
 				</div>
-				<div class="p-2 col-md-3 col-6 d-flex flex-column">
+				<div class="p-2 col-md-3 col-6 d-flex flex-column text-center">
 					<h5>Клиентам</h5>
 					<a href="#">Меню</a>
 					<a href="#">Акции</a>
 					<a href="#">Купоны</a>
 				</div>
-				<div class="p-2 col-md-3 col-6 d-flex flex-column">
+				<div class="p-2 col-md-3 col-6 d-flex flex-column text-center">
 					<h5>Мы рядом</h5>
-					<a href="#" class="d-flex align-items-center">
+					<a href="#" class="d-flex align-items-center justify-content-center">
 						<img src="/image/imgvk.png" alt="" width="16px" class="me-1 align-self-center">
 						<p class="m-0 align-self-center">Вконтакте</p>
 					</a>
-					<a href="#" class="d-flex align-items-center">
+					<a href="#" class="d-flex align-items-center justify-content-center">
 						<img src="/image/imgtg.png" alt="" width="16px" class="me-1 align-self-center">
 						<p class="m-0 align-self-center">Телеграм</p>
 					</a>
 				</div>
-				<div class="p-2 col-md-3 col-6 d-flex flex-column">
+				<div class="p-2 col-md-3 col-6 d-flex flex-column text-center">
 					<h5>Контакты</h5>
-					<a href="#" class="align-items-center">
-						<div class="d-flex align-items-center">
+					<a href="#" class="align-items-center justify-content-center">
+						<div class="d-flex align-items-center justify-content-center">
 							<img src="/image/imgemail.png" alt="" width="16px" class="me-1 align-self-center">
 							<p class="m-0 align-self-center">Почта:</p>
 						</div>
 						<p class="m-0">ilia.kozlov.y@mail.ru</p>
 					</a>
-					<a href="#" class="align-items-center">
-						<div class="d-flex align-items-center">
+					<a href="#" class="align-items-center justify-content-center">
+						<div class="d-flex align-items-center justify-content-center">
 							<img src="/image/imgphone.png" alt="" width="16px" class="me-1 align-self-center">
 							<p class="m-0 align-self-center">Телефон:</p>
 						</div>
--- resources/views/main/testheader.blade.php
+++ resources/views/main/testheader.blade.php
@@ -1,14 +1,15 @@
 <header class="sticky-top p-1">
 	<div class="container">
 		<div class="d-flex flex-wrap mb-2 justify-content-between desctop">
+			{{-- Левая часть хедера --}}
 			<div class="d-flex align-items-center header_div">
+				{{-- Лого и название --}}
 				<div class="d-flex align-items-center">
 					<a href="{{ route('main.index') }}" class="a_logo d-flex align-items-center">
 						<img src="/image/icon.png" alt="logo" class="logo me-2" width="40px">
 						<h1 class="me-5 mb-0 company_name">Chicken Burger</h1>
 					</a>
 				</div>
-				
                 <!-- Список ссылок -->
                 <ul class="nav nav-pills nav-links d-none d-lg-flex">
                     <li class="nav-item me-5 fs-5"><a href="{{ route('main.index') }}#menu" class="nav_bar">Меню</a></li>
@@ -17,14 +18,15 @@
                     <li class="nav-item me-5 fs-5"><a href="{{ route('main.map') }}" class="nav_bar">Рестораны</a></li>
                 </ul>
 			</div>
-			  <!-- Иконка бургер-меню -->
+			<!-- Иконка бургер-меню -->
 			<div class="burger-menu align-items-center d-lg-none m-0 d-flex">
+				{{-- Кнопка бургер-меню --}}
 				<button type="button" class="btn p-0" id="burgerButton">
 					<img src="/image/burger_menu.png" alt="Burger Menu" class="burger-icon" width="50px">
 				</button>
-				<!-- Меню -->
-				<div id="menu" class="menu">
-					<div class="d-flex align-items-end flex-column me-4 menu2">
+				<!-- бургер-меню -->
+				<div id="Bmenu" class="Bmenu">
+					<div class="d-flex align-items-end flex-column me-4 Bmenu2">
 						<button type="button" class="btn p-0 mt-3 mb-3" id="burgerButton2">
 							<img src="/image/burger_menu.png" alt="Burger Menu" class="burger-icon" width="50px">
 						</button>
@@ -97,40 +99,35 @@
 				</ul>
 			</div>
 		</div>
-		
-		<!-- Скрытый список ссылок для мобильных устройств -->
-		{{-- <ul class="nav nav-pills nav-links-mobile d-lg-none">
-            <li class="nav-item me-5 fs-5"><a href="{{ route('main.index') }}#menu" class="nav_bar">Меню</a></li>
-            <li class="nav-item me-5 fs-5"><a href="{{ route('coupons.index') }}" class="nav_bar">Купоны</a></li>
-            <li class="nav-item me-5 fs-5"><a href="{{ route('main.promo') }}" class="nav_bar">Акции</a></li>
-            <li class="nav-item me-5 fs-5"><a href="{{ route('main.map') }}" class="nav_bar">Рестораны</a></li>
-        </ul> --}}
 	</div>
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 	<script>
 		// Дождемся полной загрузки документа
-$(document).ready(function() {
-    // Скрываем список ссылок для мобильных устройств при загрузке страницы
-    $('.nav-links-mobile').hide();
+		$(document).ready(function() {
+			// Скрываем список ссылок для мобильных устройств при загрузке страницы
+			$('.nav-links-mobile').hide();
 
-    // Добавляем обработчик события клика на иконку бургер-меню
-    $('.burger-icon').click(function(event) {
-        // Переключаем видимость списка ссылок для мобильных устройств
-        $('.nav-links-mobile').slideToggle();
-    });
-});
+			// Добавляем обработчик события клика на иконку бургер-меню
+			$('.burger-icon').click(function(event) {
+				// Переключаем видимость списка ссылок для мобильных устройств
+				$('.nav-links-mobile').slideToggle();
+			});
+		});
 
-$(document).ready(function(){
-        // При нажатии на кнопку бургера
-        $("#burgerButton").click(function(){
-            // Отображаем или скрываем меню
-            $("#menu").toggle();
-        });
-        $("#burgerButton2").click(function(){
-            // Отображаем или скрываем меню
-            $("#menu").toggle();
-        });
-    });
+
+		// Открытие/закрытие бургер-меню
+		$(document).ready(function(){
+        	// При нажатии на кнопку бургера
+			$("#burgerButton").click(function(){
+				// Отображаем или скрываем меню
+				$("#Bmenu").toggle();
+			});
+			// При нажатии на кнопку бургера2
+			$("#burgerButton2").click(function(){
+				// Отображаем или скрываем меню
+				$("#Bmenu").toggle();
+			});
+		});
 
 	</script>
 </header>
